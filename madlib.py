cba0753ad333fe5a1d90559c5ba1f1a7a1cdb29e
Leandro-Luz-Costa <leandro_luz11@hotmail.com>
2025-04-14 13:54:26 -0300

    Madlib

---
+++ madlib.py
@@ -0,0 +1,8 @@
+#Madlib básico concatenação de strings
+
+adj = input("Adjetivo: ")
+verb = input("Verbo: ")
+verb2 = input("Verbo: ")
+nome = input("Nome: ")
+
+madlib = print(f"Olá meu nome é {nome}! Hoje vou sair para {verb}, vou {verb2} bastante, mas sei que vai ser {adj}!!")
